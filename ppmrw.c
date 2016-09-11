573938de0933bf26849564aa2e4fbba9961ee08b
Anthony Black <anthony.r.black13@gmail.com>
2016-09-11 14:51:40 -0700

    Added error checking for number of inputs the user provides.

--- ppmrw.c
+++ ppmrw.c
@@ -9,7 +9,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
+int main(int argc, char *argv[]) {
+	/*
+	simple error checking, ensures that the user provides the format to convert to,
+	the file to be read, and the file to be written.
+	*/
+	if(argc != 4){
+		printf("Incorrect number of arguments. Args given: %d\r\n", argc);
+		return EXIT_FAILURE;
+	}
 	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
