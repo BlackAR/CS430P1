29d21ae0b0c1c41cefc3bcd0bf97def2461dacb0
Anthony Black <anthony.r.black13@gmail.com>
2016-09-11 10:58:20 -0700

    Initial c file. Hello World.

---
+++ ppmrw.c
@@ -0,0 +1,15 @@
+/*
+ ============================================================================
+ Name        : ppmrw.c
+ Author      : Anthony Black
+ Description : CS430 Project 1: Converting between PPM P3 and P6
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
