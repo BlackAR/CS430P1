e682e0990c170905bb8f63ca54831ee15b682bf8
Anthony Black <anthony.r.black13@gmail.com>
2016-09-14 11:38:14 -0700

    Added rest of header reading, reading from P6, and writing in P3.

--- ppmrw.c
+++ ppmrw.c
@@ -9,33 +9,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Pixel{
+typedef struct Pixel{
 	unsigned char r, b, g;
-};
+}Pixel;
 
 int main(int argc, char *argv[]) {
 	/*
 	simple error checking, ensures that the user provides the format to convert to,
 	the file to be read, and the file to be written.
 	*/
+	int width;
+	int height;
+	int max_color;
+	int read_character;
+	FILE *input_file;
+	FILE *output_file;
+	int current_width;
+	Pixel *buffer;
 	if(argc != 4){
 		printf("Incorrect number of arguments. Args given: %d\r\n", argc);
 		return EXIT_FAILURE;
 	}
-	int read_character;
-	FILE *input_file = fopen(argv[2], "r");
+	input_file = fopen(argv[2], "r");
+	//reading the image type (should be P3 or P6)
 	read_character = getc(input_file);
 	if(read_character != 'P'){
 		printf("Input file is not in PPM format.\r\n");
 	}
 	read_character = getc(input_file);
 	printf("Type of image: P%c.\r\n", read_character);
-	FILE *output_file = fopen(argv[3], "w");
-	/*
-	buffer = buffer[height];
-	for (int i=0; i<height; i++){
-		buffer[i]= (Pixel *)malloc(width*sizeof(pixel);
-	}*/
+	read_character = getc(input_file);
+	if(read_character == '\n'){
+		printf("End of line.");
+	}
+	read_character = getc(input_file);
+	//read and print out any comment in image to console
+	if (read_character == '#'){
+		while(read_character != '\n'){
+			printf("%c", read_character);
+			read_character = getc(input_file);
+		}
+		printf("%c", read_character);
+	}
+	else{  //if there wasn't a comment, we want to go back one character so we're at the start of the line.
+		ungetc(read_character, input_file);
+	}
+	//get width, height, and max color value
+	fscanf(input_file, "%d %d\n%d\n", &width, &height, &max_color);
+	printf("width: %d, height: %d, max color: %d \r\n", width, height, max_color);
+	output_file = fopen(argv[3], "w");
+	//allocate memory for all the pixels
+	buffer = (Pixel *)malloc(width*height*sizeof(Pixel));	
+	//reading each pixel into memory for a P6 image
+	for(int i = 0; i < width*height; i++){
+		fread(&buffer[i].r, 1, 1, input_file);
+		fread(&buffer[i].g, 1, 1, input_file);
+		fread(&buffer[i].b, 1, 1, input_file);
+	}
+	//writing each pixel into file in P3 format
+	fprintf(output_file, "P3\n%d %d\n%d\n", width, height, max_color);
+	current_width = 1;
+	for(int i = 0; i < width*height; i++){
+		fprintf(output_file, "%d %d %d ", buffer[i].r, buffer[i].g, buffer[i].b);
+		if(current_width == width){
+			fprintf(output_file, "\n");
+			current_width = 1;
+		}
+		else{
+			current_width++;
+		}
+	}
+	fclose(input_file);
+	fclose(output_file);
 	return EXIT_SUCCESS;
 }
 /*
